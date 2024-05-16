c653f91d2ddcb4aa32ac771355002ce90797ecac
Ishan <146103293+Neatzy@users.noreply.github.com>
2024-05-16 10:18:29 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -2,12 +2,11 @@ public class Polynomial {
 	
 	double[] coefficients;
 	
-	public Polynomial(){
+	public Polynomial() {
 		coefficients = new double[]{0};
-
 	}
 
-	public Polynomial(double[] array){
+	public Polynomial(double[] array) {
 		coefficients = new double[array.length];
 		for (int i = 0; i < array.length; i++) {
 			coefficients[i] = array[i];
@@ -15,41 +14,35 @@ public class Polynomial {
 	}
 
 	public Polynomial add(Polynomial polynomial) {
-		
 		double[] arr = new double[Math.max(coefficients.length, polynomial.coefficients.length)];
 		for (int i = 0; i < Math.max(coefficients.length, polynomial.coefficients.length); i++) {
 			if (i < polynomial.coefficients.length && i < coefficients.length) {
 				arr[i] = polynomial.coefficients[i] + coefficients[i];
 			}
-			
 			else if(i < polynomial.coefficients.length) {
 				arr[i] = polynomial.coefficients[i];
 			}
-			
-			else if(i <coefficients.length) {
+			else if(i < coefficients.length) {
 				arr[i] = coefficients[i];
 			}
 		}
-		
 		Polynomial new_poly = new Polynomial(arr);
 		return new_poly;
 	}
 		
 	public double evaluate(double x) {
 		double result = 0;
-		for (int i = 0; i < coefficients.length; i ++) { 
+		for (int i = 0; i < coefficients.length; i++) { 
 			result += coefficients[i]*Math.pow(x, i);
 		}
-		
 		return result;
 	}
 	
 	public boolean hasRoot(double x) {
 		double result = 0;
-		for (int i = 0; i < coefficients.length; i ++) { 
+		for (int i = 0; i < coefficients.length; i++) { 
 			result += coefficients[i]*Math.pow(x, i);
 		}
-		
 		return result == 0;
 	}
 }
